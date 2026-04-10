121f3473607ee30eb5b9bf0e36d9fcace4f6c200
Romannn9 <romanbusinesscon@gmail.com>
2026-04-10 04:30:14 +0300

    Update vite.config.ts
    import { defineConfig } from 'vite'
    
    export default defineConfig({
      base: '/Digitalizedemo/',
    })

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: '/Digitalizedemo/',
+})
 import {defineConfig, loadEnv} from 'vite';
+
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
