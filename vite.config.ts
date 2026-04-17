becd14e32dd1b998e18b7b82ada29d69b6afc1e5
Kadarus <cat.kadarus@gmail.com>
2026-04-17 21:20:39 +0300

    Fix package.json and Vİte config.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite'
-export default defineConfig({
-  base: '/Digitalizedemo/',
-})
 import {defineConfig, loadEnv} from 'vite';
 
 
